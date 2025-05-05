c11eeb2e1d9b9a96b0f578cb54293cb52c4f81b2
Patrick Luo <63366848+luojunlin1223@users.noreply.github.com>
2025-05-06 00:38:27 +0800

    update

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "imgui"
     kind "StaticLib"
     language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,14 +25,12 @@ project "imgui"
 
     filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
+		
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
+
 
 	filter "configurations:Debug"
 		runtime "Debug"
